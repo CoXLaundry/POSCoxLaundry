eb5101e5c9878cec8d25f195ba3382ebfd51eb7e
CoXLaundry <coxlaundry0808@gmail.com>
2026-08-29 15:03:04 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cox-pos-v3'; // Naikkan versi setiap kali ada perubahan file
+const CACHE_NAME = 'cox-pos-v4'; // Naikkan versi setiap kali ada perubahan file
 
 // Aset inti (wajib ada agar app shell tetap tampil saat offline)
 const urlsToCache = [
